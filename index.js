261635c05eac002bb8c26024d6676ed008026701
estebansant <estebansant30@gmail.com>
2022-01-04 16:15:45 -0300

    getting close to the final version of the sidebar

--- index.js
+++ index.js
@@ -6,17 +6,10 @@ let footer = document.getElementById("footer");
 tick.addEventListener("click", openNav);
 
 function openNav() {
-    if (openNav == true) { 
-        content.style.opacity = "0.5";
-        head.style.opacity = "0.5";
-        footer.style.opacity = "0.5";
-        sidebar.style.right = "0px";
-    } else {
-        content.style.opacity = "1";
-        head.style.opacity = "1";
-        footer.style.opacity = "1";
-        sidebar.style.right = "-300px";
-    } 
+    content.style.opacity = "0.5";
+    head.style.opacity = "0.5";
+    footer.style.opacity = "0.5";
+    sidebar.style.right = "0px";
 }
 
 content.onclick = closeNav;
@@ -28,5 +21,4 @@ function closeNav() {
     content.style.opacity = "1";
     head.style.opacity = "1";
     footer.style.opacity = "1";
-    
 }
