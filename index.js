93a4ce45381de1c50eb2310f6bcc4689ee7aa52d
estebansant <estebansant30@gmail.com>
2022-01-04 16:47:56 -0300

    sidebar finished (for now)

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-let tick = document.getElementById("check");
 let sidebar = document.getElementById("sidebar");
 let content = document.getElementById("content");
-let head = document.getElementById("head");
+let head = document.getElementById("op");
 let footer = document.getElementById("footer");
-tick.addEventListener("click", openNav);
 
 function openNav() {
-    content.style.opacity = "0.5";
     head.style.opacity = "0.5";
+    content.style.opacity = "0.5";
     footer.style.opacity = "0.5";
     sidebar.style.right = "0px";
 }
 
-content.onclick = closeNav;
-head.onclick = closeNav;
-footer.onclick = closeNav;
+content.addEventListener("click", closeNav);
+head.addEventListener("click", closeNav);
+footer.addEventListener("click", closeNav);
 
 function closeNav() {
     sidebar.style.right = "-300px";
