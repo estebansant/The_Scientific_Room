d784f5114851c314a3181cf2f92edadf9a954cba
estebansant <estebansant30@gmail.com>
2022-02-22 09:58:49 -0300

    finished the legal advise page

---
+++ main_website/videos/random.js
@@ -0,0 +1,7 @@
+function video (image, language, title, creator, length){
+    this.image = image;
+    this.language = language;
+    this.title = title;
+    this.creator = creator;
+    this.length = length;
+}
